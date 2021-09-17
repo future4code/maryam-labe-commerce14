32f29d4b5b496ab401c754c3415246926979b4d3
Mara Priscilla Barros Souza <priscillabarrossouza@gmail.com>
2021-09-17 15:14:43 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import styled from "styled-components"
 
-class App extends React.Component {
-state =  {
 
-
-}
-
-render () {
-  return (
-    
-  )
-}
-}
 export default App;
